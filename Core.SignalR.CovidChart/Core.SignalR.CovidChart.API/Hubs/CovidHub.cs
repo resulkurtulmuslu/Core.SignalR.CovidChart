18c3f6f19ab9d3520548925afd0a30339bda96e8
Resul Kurtulmuşlu <resulkurtulmuslu@outlook.com>
2021-04-20 00:38:03 +0300

    create project, create database, create hub, create service

---
+++ Core.SignalR.CovidChart/Core.SignalR.CovidChart.API/Hubs/CovidHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Core.SignalR.CovidChart.API.Hubs
+{
+    public class CovidHub : Hub
+    {
+        public async Task GetCovidList()
+        {
+            await Clients.All.SendAsync("ReceiveCovidList","data");
+        }
+    }
+}
---
+++ Core.SignalR.CovidChart/Core.SignalR.CovidChart.API/Models/Covid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.SignalR.CovidChart.API.Models
+{
+    public enum ECity
+    {
+        Istanbul = 1,
+        Ankara = 2,
+        Izmir = 3,
+        Sivas = 4,
+        Giresun = 5
+    }
+
+    public class Covid
+    {
+        public int Id { get; set; }
+
+        public ECity City { get; set; }
+
+        public int Count { get; set; }
+
+        public DateTime CovidDate { get; set; }
+    }
+}
